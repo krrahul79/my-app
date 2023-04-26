656dbe7e8eb3d77002678b1b71b5735b8ba734aa
Rahul <kr.rahul79@gmail.com>
2023-04-26 23:56:21 +0100

    Updating the root componet

--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
 //component means creating a function
 //export component to use it
 //We can have multiple components but only one root component
+//root component is only one
+//always selfclose the react component while rendering
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const Greeting = () =>{
     return <h2>Hi! Welcome</h2>;
 }
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<Greeting/>);
